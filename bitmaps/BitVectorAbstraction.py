17f4cd00561fee3fe0143e1abbb80b448194adb7
Collin <collin.lehrian@gmail.com>
2019-06-11 09:58:26 -0400

    Updating bitvector files

---
+++ bitmaps/BitVectorAbstraction.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 10 16:51:19 2019
+
+@author: clehrian
+"""
+
---
+++ bitmaps/RoaringImplementation.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 10 14:14:58 2019
+
+@author: clehrian
+"""
+
+from roaringbitmap import RoaringBitmap
+
+data = np.array([4,5,4,5,4,5,4,5,4,5,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4])
+
+c = RoaringBitmap()
