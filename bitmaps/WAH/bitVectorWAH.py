400d198e0ee510ebf6a805e3937102b5daae2e65
McClurg <josiah_mcclurg@campus.tayloru.edu>
2019-06-20 17:45:15 -0400

    0. Added comments throughout 1. Changed name of class to bitVectorWAH 2. Updated wordSizeInBits to be a static attribute rather than an instance attribute 3. Separated iterator functions into separate class: WAHIterator 4. Separated append functions into separate sub class: appendableWAH 5. Renamed setBitInActiveWord to setBitInAppendWord 6. See other comments in my e-mail.

--- bitmaps/WAH/bitVectorWAH.py
+++ bitmaps/WAH/bitVectorWAH.py
@@ -1,248 +1,52 @@
 import numpy as np
+from WAHStorageWordBuilder import WAHStorageWordBuilder
+from WAHStorageWordIterator import WAHStorageWordIterator
 
-class bitVector:
+class bitVectorWAH(object):
+    
+    ## Initialize the bitVector with an appropriate storage.
+    def __init__(self, wahStorage = WAHStorageWordBuilder()):
+        self.wahStorage = wahStorage
 
     #######################################################################
     #                         Helper Functions                            #
     #######################################################################
-
-    def getFirstLenRemaining(self):
-        #allows us to initialize the lenRemaining of the first word
-        if ( self.storage[self.activeWordIndex] >> np.uint64(63) ) == 1:
-            #if fill word get the length of it
-            self.lenRemaining = self.storage[self.activeWordIndex] & ~( np.uint64(3) << np.uint64(62) )
-        else:
-            #if literal just set to 0
-            self.lenRemaining = 0;
-
-    def getLen(self):
-        #gets the length of the run word 
-        return (self.storage[self.activeWordIndex] & ~(np.uint64(3) << np.uint64(62)))
-
-    def moveIteratorForward(self,numWords):
-        
-        while(numWords > 0):
-            # Because the length remaining does not include current word, then lenRemaining == 0 means that we are finished with the current run or literal.
-            if self.lenRemaining == 0:
-                self.activeWordIndex += 1
-                # If new active word is a run, set length remaining accordingly. 
-                if ( self.storage[self.activeWordIndex] >> np.uint64(63) ) == 1:
-                    self.lenRemaining = self.storage[self.activeWordIndex] & ~( np.uint64(3) << np.uint64(62) )
-                # If new active word is a literal, set length to one
-                else:
-                    self.lenRemaining = 1
-            
-            # You processed a word, so decrease the length remaining.
-            self.lenRemaining -= 1
-            numWords -= 1
-            
-    def rewind(self):
-        #goes to the first word of the BV
-        self.activeWordIndex = 0
-
-    def getTotalLength(self):
-
-        totalLength = 0
-
-        for i in range(len(self.storage)):
-            runOrLiteral = self.storage[i] >> np.uint64(63)
-            if runOrLiteral == 1:
-                #Is run
-                length = self.storage[i] & ~(np.uint64(3) << np.uint64(62))
-                totalLength += length
-            else:
-                #Is literal
-                totalLength += 1
-        return totalLength
-
-
-    def printBitVector(self):
-        for i in self.storage:
-            print np.binary_repr(i, width = 64)
-
-    def isLiteral(self, word):
-        return word >> self.wordSizeInBits - np.uint64(1) == 0
-
-    def setBitInActiveWord(self,pos,bit):
-        if bit == 0:
-            self.storage[self.appendWordIndex] &= ~(np.uint64(1)<<(np.uint64(self.wordSizeInBits - 2) - np.uint64(pos)))
-        else:
-            self.storage[self.appendWordIndex] |=  (np.uint64(1)<<(np.uint64(self.wordSizeInBits - 2) - np.uint64(pos)))
     
-    # Make sure that lenInWords can fit in the current storage
-    def ensureStorageFits(self, lenInWords):
-        if self.storage.size < lenInWords:
-            # TODO: In the future, this should do something a bit more efficient, like growing in blocks of size 1.5*current length or something like that.
-            self.storage.resize(lenInWords)       
-
-    def appendWord(self, word):
-        #appends a word to a bitVector based on logical operations
-        #Will also check if word is a run and send it to appendRun which will add it
-    
-        #Case 1: word is run
-        if word == ~(np.uint64(1) << np.uint64(63)) or word == np.uint64(0):
-            runType = (word >> np.uint64(62)) & np.uint64(1)
-            getLength = np.uint64(1) << np.uint64(63)
-            getLength += np.uint64(1) << np.uint64(62)
-            length = word & ~(getLength)
-            
-            self.appendRun(runType,length)
-            
-        #Case 2: word is literal
-        else:
-            #Checks if bitVector needs to be expanded
-            self.ensureStorageFits(self.activeWordIndex + 1)
-            #Sets word in correct spot in bitVector
-            self.storage[self.activeWordIndex] += word
-            #Iterates the index to the new spot
-            self.activeWordIndex += 1
-
-    def appendRun(self, runType, length):
-    
-        word = np.uint64(1) << np.uint64(63)
-        word += np.uint64(runType) << np.uint64(62)
-        word += length
-        
-        prevRunType = self.storage[self.activeWordIndex - 1] & (np.uint64(1) << np.uint64(62))
-        
-        if prevRunType == runType:
-            #if we do not overrun by adding the length than just add the length
-            # 4611686018427387903 is the max length that our fill words can represent
-            if (self.getLen + length) < 4611686018427387903:
-                self.storage[self.activeWordIndex - 1] += length
-            
-            #If run becomes too large it will create an new run in the next spot in storage                
-            else:
-                self.ensureStorageFits(self.activeWordIndex + 1)
-                newRun = np.uint64(1) << np.uint64(63)
-                newRun += np.uint64(runType) << np.uint64(62)
-                newRun += 1
-                self.storage[self.activeWordIndex] = newRun
-            
-        else:
-            self.ensureStorageFits(self.activeWordIndex + 1)        
-            
-            self.storage[self.activeWordIndex] = word
-            
-            self.activeWordIndex += 1
-    
-    #######################################################################
-    #                           Methods                                   #
-    #######################################################################
-        
-    def __init__(self):
-
-        #Creates bitVector
-        self.storage = np.zeros(1,dtype = np.uint64)
-        
-        #Sets wordSize
-        self.wordSizeInBits = np.uint64(64)
-        
-        #rows in word
-        self.numRows = 0
-
-        # This implementation is designed so that the partial literal length is always < wordSizeInBits - 1.
-        # See also comments on activeWordIndex.
-        #length of last literal
-        self.partialLiteralLength = 0
-        
-        #Sets numWords to zero
-        self.numWords = 1 # length of storage
-        
-        # Important: This implementation is designed such that the active word is always a literal.
-        # Another literal is always added if the active word gets full.
-        self.activeWordIndex = 0 # this is the index of the active word
-        
-        self.appendWordIndex = 0 # Location of the last word in the bitvector. The append function will *only* work with this index
-
-        #lenRemaining is the lenght of the fill word
-        self.lenRemaining = 0
-        
-        # Location of the last word in the bitvector. The append function will *only* work with this index.
-        self.appendWordIndex = 0 
-        
-        #totallength
-        self.totalLength = 0
-    
-    def append(self,bit):
-        self.numRows += 1
-        
-                   
-        #adds the bit to the literal in the last space            
-        self.setBitInActiveWord(self.partialLiteralLength,bit)
-        
-        self.partialLiteralLength += 1
-        
-        # Done:
-        # Grow the storage and move the active word if needed.
-        if self.partialLiteralLength == (self.wordSizeInBits - 1):
-
-            #check for merge back. Note that active word is always a literal.
-            if self.storage[self.appendWordIndex] == 0 or ~(self.storage[self.appendWordIndex] | np.uint64(1) << np.uint64(self.wordSizeInBits - 1)) == 0:
-                print 'Found a run. Checking if we need to merge this back into the previous word.'
-                #Sets active word to fill of type bit and size one
-                self.storage[self.appendWordIndex] = np.uint64(1)<<(np.uint64(self.wordSizeInBits - 1)) | (np.uint64(bit) << (np.uint64(self.wordSizeInBits - 2))) | np.uint64(1)
-                
-                # Not done:
-                #FIXME: need to check overflow for size of run
-                
-                # Do the merge if last word is the right type
-                if self.appendWordIndex > 0 and self.storage[self.appendWordIndex - 1] >> np.uint64(62) == self.storage[self.appendWordIndex] >> np.uint64(62):
-                    self.storage[self.appendWordIndex - 1] += np.uint64(1)
-                    self.storage[self.appendWordIndex] = np.uint64(0)
-                    self.partialLiteralLength = 0
-                
-                # Don't do the merge if the last word is the wrong type.
-                else:
-                    self.ensureStorageFits(self.numWords + 1)
-                    self.partialLiteralLength = 0
-                    self.appendWordIndex += 1
-                    self.numWords += 1
-                
-            #Checks if not fill and needs to be expanded
-            else:
-                #print 'Hey booboo'
-                self.ensureStorageFits(self.numWords + 1)
-                self.partialLiteralLength = 0
-                self.appendWordIndex += 1
-                self.numWords += 1
-        
+    ## NOTE: I partially modified this, to use the new iterator. You need to fix the rest.
     def xor(self, other):
-
         #Checks if Bit Vectors are same size (throws error if not)
-        if self.getTotalLength() != other.getTotalLength():
-            print "Not Same Size. Error."
-            return
+        if self.wahStorage.totalLength != other.wahStorage.totalLength:
+            raise Exception("Not the same size.")
         
-        #Sets activeWordIndex to zero for both self and other
-        self.activeWordIndex = 0
-        other.activeWordIndex = 0
+        # These are the iterators which
+        me = WAHStorageWordIterator(self)
+        you = WAHStorageWordIterator(other)
 
         #Creates new bitVector to hold xor operation
-        new = bitVector()
-        self.totalLength = self.getTotalLength()
+        new = WAHStorageWordBuilder()
         
-        #FIXME: Not sure how to iterate this 
-        while self.totalLength:
+        while not (me.isDone() or you.isDone()):
+            (meActiveWord, meLenRemaining) = me.current()
+            (youActiveWord, youLenRemaining) = you.current()
             
-            #checks for cases
-            selfCheck = self.storage[self.activeWordIndex] >> np.uint64(63)
-            otherCheck = other.storage[other.activeWordIndex] >> np.uint64(63)
+            meLiteral = me.wahStorage.isLiteral(meActiveWord)
+            youLiteral = you.wahStorage.isLiteral(youActiveWord)
             
             #Case 1: Both are literals
-            if selfCheck == otherCheck and selfCheck == 0:
+            # NOTE: This is the one I modified.
+            if meLiteral and youLiteral:
                 #XOR operation done between the two current words
-                newWrd = self.storage[self.activeWordIndex] ^ other.storage[other.activeWordIndex]
+                newWrd = meActiveWord ^ youActiveWord
                 #Adds the XOR'ed word to the new bitVector
                 new.appendWord(newWrd)
-                #Since we used these two words, we subtract one from each of the total lengths 
-                self.totalLength -= 1
+                
                 #Moves the iterator forward for each bitVector (since both literals we move each by one word)
-                self.moveIteratorForward(1)
-                other.moveIteratorForward(1)
+                me.moveIteratorForward(1)
+                you.moveIteratorForward(1)
                 
             #Case 2: Both are fills
-            if selfCheck == otherCheck and selfCheck == 1:
+            # FIXME: I only modified the if statement. You need to fix up the rest.
+            elif (not meLiteral) and (not youLiteral):
                 #Gets the length of the two fills 
                 getLength = np.uint64(1) << np.uint64(63)
                 getLength += np.uint64(1) << np.uint64(62)
@@ -276,7 +80,8 @@ class bitVector:
 
 
             #Case 3: One is literal and one is run
-            if selfCheck != otherCheck and (selfCheck == 0 or otherCheck == 0):
+            # FIXME: I only modified the if statement. You need to fix up the rest.
+            else:
                 #If self is a run
                 if selfCheck == 1:
                     #Gets self's runtype
@@ -318,6 +123,7 @@ class bitVector:
         #Sets the bitVector's storage equal to the XOR'ed bitVector
         self.storage = new.storage
 
+    ## FIXME: I have not modified this to use the new iterator. You need to do so. See xor function for hints.
     def Or(self, other):
         
         new = bitVector()
@@ -420,7 +226,7 @@ class bitVector:
                         
                         loopLen -= other.lenRemaining
                     
-                
+    ## FIXME: I have not modified this to use the new iterator. You need to do so. See xor function for hints.
     def AND(self, other):
         ############ REMINDER #############
         #   anything '&' with 0 is zero   #
